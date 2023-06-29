ccc3e973c07d8d2200b9070a3cb0212b38db176f
Daria Vasilega <dariavasilega@gmail.com>
2023-06-29 13:40:03 +0300

    Resolves #11: Project Structure Amendments.
    - Moved `lang` and `migrations` directories under the newly created `./resources`.
    - Added directory for fixtures.
    - Changed configs so that the application could work with the new paths of these directories.
    - Changed the way of retrieving default locale and allowed locales in the Locale class. At the moment they try from config and if not - take from constants.

---
+++ resources/i18n/en_US/validation.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'required' => ':Attribute must not be empty.',
+    'format' => ':Attribute must be in :format format.',
+    'unique' => 'This :attribute is already registered.',
+    'in' => ':Attribute must be one of the listed values: "%s".',
+    'min.string' => ':Attribute must be longer than :min character(s).',
+    'max.string' => ':Attribute must be less than :max character(s).',
+];
